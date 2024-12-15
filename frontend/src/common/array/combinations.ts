26393fd0319b56decb35c2fc3e7c5c1e496d126e
EffortlessHome <167689088+EffortlessHome@users.noreply.github.com>
2024-12-15 10:00:40 -0500

    Delete frontend directory

--- frontend/src/common/array/combinations.ts
+++
@@ -1,9 +0,0 @@
-export function getAllCombinations<T>(arr: T[]) {
-  return arr.reduce<T[][]>(
-    (combinations, element) =>
-      combinations.concat(
-        combinations.map((combination) => [...combination, element])
-      ),
-    [[]]
-  );
-}
--- frontend/src/entrypoints/authorize.ts
+++
@@ -1,8 +0,0 @@
-// Compat needs to be first import
-import "../resources/compatibility";
-import "../auth/ha-authorize";
-
-import("../resources/ha-style");
-import("@polymer/polymer/lib/utils/settings").then(
-  ({ setCancelSyntheticClickEvents }) => setCancelSyntheticClickEvents(false)
-);
